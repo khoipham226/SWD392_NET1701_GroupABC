ce190c3d3f41198d2f27531245eb2184bf62da3e
khoipham226 <113128599+khoipham226@users.noreply.github.com>
2024-05-25 18:52:11 +0700

    update config

--- SWDProject_BE/SWDProject_BE/Program.cs
+++ SWDProject_BE/SWDProject_BE/Program.cs
@@ -1,11 +1,37 @@
+using SWDProject_BE.AppStarts;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Install AutoMapper
+builder.Services.ConfigureAutoMapper();
+// Install DI and dbcontext
+builder.Services.InstallService(builder.Configuration);
+// Swagger config
+builder.Services.ConfigureSwaggerServices("SWDProject");
+builder.Services.ConfigureAuthService(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	c.IncludeXmlComments(xmlPath);
+});
+// Add CORS
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			   .AllowAnyMethod()
+			   .AllowAnyHeader();
+	});
+});
 
 var app = builder.Build();
 
@@ -15,9 +41,10 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
+app.UseCors();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
