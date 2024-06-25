8029aa197a19728b3f7ffd48ca592618d5467cf8
duythuan123 <146064542+duythuan123@users.noreply.github.com>
2024-06-25 09:24:39 +0700

    Update UpdateDB.sql

--- UpdateDB.sql
+++ UpdateDB.sql
@@ -1,3 +1,12 @@
+-- Step 0: create [ExchangedProduct]
+CREATE TABLE [dbo].[ExchangedProduct] (
+    [Id] INT IDENTITY(1,1) PRIMARY KEY,
+    [ExchangeId] INT NOT NULL,
+    [ProductId] INT NOT NULL,
+    FOREIGN KEY ([ExchangeId]) REFERENCES [dbo].[Exchanged]([Id]), 
+    FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Product]([Id])
+) ON [PRIMARY];
+
 -- Step 1: Delete all rows from the table (Excute each row manually)
 DELETE FROM [SWD392_DB].[dbo].[ExchangedProduct];
 DELETE FROM [SWD392_DB].[dbo].[Post];
