89e03e86fde5791943c3bc03aecec27ae5d7e6ab
duythuan123 <146064542+duythuan123@users.noreply.github.com>
2024-07-08 23:22:14 +0700

    Modify get product by id, get sub by category id
    - Get subcategory only get those with status true
    - Get product by id now have user img

--- SWDProject_BE/BusinessLayer/Services/Implements/SubcategoryService.cs
+++ SWDProject_BE/BusinessLayer/Services/Implements/SubcategoryService.cs
@@ -148,7 +148,7 @@ namespace BusinessLayer.Services.Implements
             var category = await _unitOfWork.Repository<Category>().GetById(categoryId);
             if(category != null)
             {
-                var subcategory = await _unitOfWork.Repository<SubCategory>().GetAll().Where(s => s.CategoryId == categoryId).ToListAsync();
+                var subcategory = await _unitOfWork.Repository<SubCategory>().GetAll().Where(s => s.CategoryId == categoryId && s.Status == true).ToListAsync();
                 var result = _mapper.Map<List<SubcategoryResponseModel>>(subcategory);
                 return result;
             }                   
